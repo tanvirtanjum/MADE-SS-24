8e7517a45191bfca780b1fd0ca8a73d93f56a50b
Tanvir Tanjum Shourav <tanjumtanvir@gmail.com>
2024-05-26 20:22:25 +0200

    Clean Code

--- project/pipeline.py
+++ project/pipeline.py
@@ -8,11 +8,15 @@ node = 0
 
 class Pipeline():
     PipelineData : object
+    url : str
+    dropColumns : object
+    selectedColumns : object
     
     def __init__(self, PipelineData = None):
         self.PipelineData = PipelineData
         
-    def csvFetch(self, url : str):
+        
+    def csvFetch(self):
         global node
         
         if node == 0:
@@ -21,50 +25,52 @@ class Pipeline():
         if node > 0:
             progress("Fetching Data from Source", node)
         try:
-            data = pd.read_csv(url)
-            dataFrame = pd.DataFrame(data)
-            return dataFrame
+            data = pd.read_csv(self.url)
+            self.PipelineData = pd.DataFrame(data)
         except:
             error("Data couldn't fetched...")
             fail("Aborted...")
             sys.exit(0)
-
-    def columnSelector(self, data : object, dropColumns : object, selectedColumns : object):
-        if len(dropColumns) > 0:
-            data.drop(dropColumns, inplace = True, axis = 1) 
-        if len(selectedColumns) > 0:
-            data = data[selectedColumns]
             
-        return data
 
-    def rowSelector(self, data : object, row : str, value : str):
-        return data[data[row] == value]
+    def columnSelector(self):
+        if len(self.dropColumns) > 0:
+            self.PipelineData.drop(self.dropColumns, inplace = True, axis = 1) 
+        if len(self.selectedColumns) > 0:
+            self.PipelineData = self.PipelineData[self.selectedColumns]
+            
 
-    def renameColumn(self, data : object, renameColumns : object):
-        data.rename(columns = renameColumns, inplace = True)
-        return data
+    def rowSelector(self, row : str, value : str):
+        self.PipelineData = self.PipelineData[self.PipelineData[row] == value]
+        
 
-    def fixYear(self, data : object):
-        data = data
-        data["Year"] = data["Year"].str[1:]
-        return data
+    def renameColumn(self, renameColumns : object):
+        self.PipelineData.rename(columns = renameColumns, inplace = True)
+        
 
-    def dropNull(self, data : object):
+    def fixYear(self):
+        self.PipelineData["Year"] = self.PipelineData["Year"].str[1:]
+        
+
+    def dropNull(self):
         primary("Filtering rows...")
-        return data.dropna(how = "any", axis = 0) 
+        self.PipelineData = self.PipelineData.dropna(how = "any", axis = 0) 
+        
 
-    def meltTable(self, data : object, keep : object, melt : object):
+    def meltTable(self, keep : object, melt : object):
         primary("Processing Data...")
-        return pd.melt(data, id_vars=keep, value_vars=melt, ignore_index=True)
+        self.PipelineData =  pd.melt(self.PipelineData, id_vars=keep, value_vars=melt, ignore_index=True)
+        
 
     def dataLeftJoin(self, left : object, right : object, key : object, leftSufx : str, rightSufx : str):
         primary("Joining Data...")
-        return pd.merge(left, right, how ='left', on = key, suffixes=(leftSufx, rightSufx)) 
-
-    def csvToSQLite(self, data : object, savingPath : str, sqliteFileName : str, sqliteTableName : str):
+        self.PipelineData = pd.merge(left, right, how ='left', on = key, suffixes=(leftSufx, rightSufx))
+        
+        
+    def csvToSQLite(self, savingPath : str, sqliteFileName : str, sqliteTableName : str):
         try:
             conn = sqlite3.connect(savingPath+sqliteFileName)
-            data.to_sql(sqliteTableName, conn, if_exists='replace', index=False)
+            self.PipelineData.to_sql(sqliteTableName, conn, if_exists='replace', index=False)
             complete("Data Exported... [Path: "+savingPath+"\\"+sqliteFileName+"]")
             conn.close()  
             success("End...")
@@ -74,7 +80,7 @@ class Pipeline():
             progress2("Resolving Path...")
             savingPath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)+"\n")) + "\\data\\"
             conn = sqlite3.connect("../data/"+sqliteFileName)
-            data.to_sql(sqliteTableName, conn, if_exists='replace', index=False)
+            self.PipelineData.to_sql(sqliteTableName, conn, if_exists='replace', index=False)
             successDim("Path Resolved...")
             complete("Data Exported... [Path: "+savingPath+"\\"+sqliteFileName+"]")
             conn.close()  
@@ -84,39 +90,39 @@ class Pipeline():
 def main():
     # 1st Data Source
     p1 = Pipeline()
-    url1 = "https://opendata.arcgis.com/datasets/4063314923d74187be9596f10d034914_0.csv"
-    dropColumns1 = ["ObjectId", "Indicator", "Source", "CTS_Code", "CTS_Name", "CTS_Full_Descriptor",  "F2000","F2001","F2002","F2003","F2004","F2005","F2006","F2007","F2008","F2009"]
-    selectedColumns1 = ["Country","ISO3","Unit", "F2010","F2011","F2012","F2013","F2014","F2015","F2016","F2017","F2018","F2019","F2020"]
+    p1.url = "https://opendata.arcgis.com/datasets/4063314923d74187be9596f10d034914_0.csv"
+    p1.dropColumns = ["ObjectId", "Indicator", "Source", "CTS_Code", "CTS_Name", "CTS_Full_Descriptor",  "F2000","F2001","F2002","F2003","F2004","F2005","F2006","F2007","F2008","F2009"]
+    p1.selectedColumns = ["Country","ISO3","Unit", "F2010","F2011","F2012","F2013","F2014","F2015","F2016","F2017","F2018","F2019","F2020"]
     renameColumns1 = {"variable":"Year", "value":"Temperature"}
-    p1.PipelineData = p1.csvFetch(url1)
-    p1.PipelineData = p1.columnSelector(p1.PipelineData, dropColumns1, selectedColumns1)
-    p1.PipelineData = p1.meltTable(p1.PipelineData, ['ISO3', "Country"], ["F2010","F2011","F2012","F2013","F2014","F2015","F2016","F2017","F2018","F2019","F2020"])
-    p1.PipelineData = p1.renameColumn(p1.PipelineData, renameColumns1)
-    p1.PipelineData = p1.fixYear(p1.PipelineData)
+    p1.csvFetch()
+    p1.columnSelector()
+    p1.meltTable(['ISO3', "Country"], ["F2010","F2011","F2012","F2013","F2014","F2015","F2016","F2017","F2018","F2019","F2020"])
+    p1.renameColumn(renameColumns1)
+    p1.fixYear()
 
     # 2nd Data Source
     p2 = Pipeline()
-    url2 = "https://opendata.arcgis.com/datasets/b13b69ee0dde43a99c811f592af4e821_0.csv"
-    dropColumns2 = ["ObjectId", "Source", "CTS_Code", "CTS_Name", "CTS_Full_Descriptor",  "F2000","F2001","F2002","F2003","F2004","F2005","F2006","F2007","F2008","F2009",]
-    selectedColumns2 = ["ISO3","Unit", "F2010","F2011","F2012","F2013","F2014","F2015","F2016","F2017","F2018","F2019","F2020"]
+    p2.url = "https://opendata.arcgis.com/datasets/b13b69ee0dde43a99c811f592af4e821_0.csv"
+    p2.dropColumns = ["ObjectId", "Source", "CTS_Code", "CTS_Name", "CTS_Full_Descriptor",  "F2000","F2001","F2002","F2003","F2004","F2005","F2006","F2007","F2008","F2009",]
+    p2.selectedColumns = ["ISO3","Unit", "F2010","F2011","F2012","F2013","F2014","F2015","F2016","F2017","F2018","F2019","F2020"]
     renameColumns2 = {"variable":"Year", "value":"Incident"}
-    p2.PipelineData = p2.csvFetch(url2)
-    p2.PipelineData = p2.rowSelector(p2.PipelineData, "Indicator", "Climate related disasters frequency, Number of Disasters: TOTAL")
-    p2.PipelineData = p2.columnSelector(p2.PipelineData, dropColumns2, selectedColumns2)
-    p2.PipelineData = p2.meltTable(p2.PipelineData, ['ISO3'], ["F2010","F2011","F2012","F2013","F2014","F2015","F2016","F2017","F2018","F2019","F2020"])
-    p2.PipelineData = p2.renameColumn(p2.PipelineData, renameColumns2)
-    p2.PipelineData = p2.fixYear(p2.PipelineData)
+    p2.csvFetch()
+    p2.rowSelector("Indicator", "Climate related disasters frequency, Number of Disasters: TOTAL")
+    p2.columnSelector()
+    p2.meltTable(['ISO3'], ["F2010","F2011","F2012","F2013","F2014","F2015","F2016","F2017","F2018","F2019","F2020"])
+    p2.renameColumn(renameColumns2)
+    p2.fixYear()
 
     # Final Data
     p3 = Pipeline()
-    p3.PipelineData = p3.dataLeftJoin(p1.PipelineData, p2.PipelineData, ["ISO3", "Year"], "_temp", "_incident")
-    p3.PipelineData = p3.dropNull(p3.PipelineData)
-
+    p3.dataLeftJoin(p1.PipelineData, p2.PipelineData, ["ISO3", "Year"], "_temp", "_incident")
+    p3.dropNull()
     # Saving Data
     targetedPath = os.path.join(os.getcwd(), "data\\")  
     fileName = "SurfaceTemperatureChangeOnClimate_relatedDisaster.sqlite"
     dbName = "Temp_Disaster"
-    p3.csvToSQLite(p3.PipelineData, targetedPath, fileName, dbName)
+    p3.csvToSQLite(targetedPath, fileName, dbName)
+    
     
 if __name__ == "__main__":
     main()
